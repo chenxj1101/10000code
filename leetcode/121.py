404b5e3216fdd5caa9778c60a9e71bc55b113175
chenxj1101 <442185473@qq.com>
2019-11-01 16:19:36 +0800

    更新下121

--- leetcode/121.py
+++ leetcode/121.py
@@ -5,7 +5,7 @@
 @Github: https://github.com/chenxj1101
 @Mail: ccj799@gmail.com
 @Date: 2019-11-01 15:08:29
-@LastEditTime: 2019-11-01 15:21:39
+@LastEditTime: 2019-11-01 16:15:35
 @Description: 买卖股票的最佳时机-简单  https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
 '''
 
@@ -18,4 +18,13 @@ def maxProfit(prices: List[int]) -> int:
         max_p = max(max_p, prices[i]-min_p)
     return max_p
 
-print(maxProfit([7,1,5,3,6,4]))
+#print(maxProfit([7,1,5,3,6,4]))
+#------------------------------------------------------------------
+def maxProfit2(prices: List[int]) -> int:
+    temp = max_profit = 0
+    for i in range(len(prices)-1):
+        temp = max(0, prices[i+1]-prices[i]+temp)
+        max_profit = max(max_profit, temp)
+    return max_profit
+
+print(maxProfit2([7,1,5,3,6,4]))
